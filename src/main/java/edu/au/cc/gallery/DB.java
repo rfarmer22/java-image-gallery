2d924de3a045a77f3db8d7135f005f7638d83b7d
Ross Farmer <rzf0030@auburn.edu>
2022-05-31 23:34:59 +0000

    Commit prior to turning in project 2.

--- src/main/java/edu/au/cc/gallery/DB.java
+++ src/main/java/edu/au/cc/gallery/DB.java
@@ -10,49 +10,128 @@ import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class DB {
 
-    private static final String dbUrl = "jdbc:postgresql://demo-database-1.c923ckbw7nvl.us-east-2.rds.amazonaws.com/image_gallery";
+    private static final String dbUrl = "jdbc:postgresql://demo-database-1.cbimem44ngio.us-east-1.rds.amazonaws.com/image_gallery";
 
     private Connection connection;
 
-    private JSONObject getSecret() {
-	String s = Secrets.getSecretImageGallery();
-	return new JSONObject(s);
-    }
+    private String getPassword() {
+	try(BufferedReader br = new BufferedReader(new FileReader("/home/ec2-user/.sql-passwd"))) {
+		String result = br.readLine();
+		return result;
+	} catch (IOException ex) {
+		System.err.println("Error opening password file. Make sure .sql-passwd exists and contains your sql password.");
+		System.exit(1);
+    		}
+		return null;
+	}
 
-    private String getPassword(JSONObject secret) {
-	return secret.getString("password");
-    }
-    
+    //Connects DB
     public void connect() throws SQLException {
 	try {
 	    Class.forName("org.postgresql.Driver");
-	    JSONObject secret = getSecret();
-	    connection = DriverManager.getConnection(dbUrl, "image_gallery", getPassword(secret));
+	    connection = DriverManager.getConnection(dbUrl, "image_gallery", getPassword());
 	} catch (ClassNotFoundException ex) {
 	    ex.printStackTrace();
 	    System.exit(1);
 	}
     }
 
-    public ResultSet execute(String query) throws SQLException {
-	PreparedStatement stmt = connection.prepareStatement(query);
+    //Reads Users
+    public void listUsers() throws SQLException {
+	PreparedStatement stmt = connection.prepareStatement("select username, password, full_name from users");
 	ResultSet rs = stmt.executeQuery();
-	return rs;
+	while(rs.next()) {
+	   System.out.println(rs.getString(1)+","+rs.getString(2)+","+rs.getString(3));
+	}
+	rs.close();
     }
 
-    public void execute(String query, String[] values) throws SQLException {
-	PreparedStatement stmt = connection.prepareStatement(query);
-	for(int i=0; i < values.length; i++)
-	    stmt.setString(i+1, values[i]);
+    //Checks if user exists
+    public boolean existingUser (String username) throws SQLException {
+        PreparedStatement stmt = connection.prepareStatement("select * from users where username = ?");
+	stmt.setString(1, username);
+	ResultSet rs = stmt.executeQuery();
+	if(rs.next() == true){
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+
+    //Adds User
+    public void addUser(String username, String password, String fullname) throws SQLException {
+	if(existingUser(username) == true){
+	   System.out.println("Error: user with username "+username+" already exists");
+	   return;
+	} else {
+	PreparedStatement stmt = connection.prepareStatement("insert into users (username, password, full_name) values (?,?,?)");
+	stmt.setString(1, username);
+	stmt.setString(2, password);
+	stmt.setString(3, fullname);
 	stmt.execute();
+	System.out.println("User "+username+" created successfully");
+	return;
+	}
     }
 
+    //Updates User
+    public void updateUser(String username, String password, String fullname) throws SQLException {
+	   System.out.println();
+	if(existingUser(username) == false){
+           System.out.println("No such user.");
+           return;
+        } else {
+	if(password.equals("") && fullname.equals("")){
+	   System.out.println("No updates needed.");
+	   return;
+	   }
+	if(password.equals("") && !fullname.equals("")){
+	   PreparedStatement stmt = connection.prepareStatement("update users set full_name =? where username=?");
+           stmt.setString(1, fullname);
+           stmt.setString(2, username);
+           stmt.execute();
+           System.out.println("User "+username+" updated successfully.");
+           return;
+	   }
+	if(!password.equals("") && fullname.equals("")){
+	   PreparedStatement stmt = connection.prepareStatement("update users set password =? where username=?");
+           stmt.setString(1, password);
+           stmt.setString(2, username);
+           stmt.execute();
+           System.out.println("User "+username+" updated successfully.");
+           return;
+           }
+	if(!password.equals("") && !fullname.equals("")){
+	   PreparedStatement stmt = connection.prepareStatement("update users set full_name =?, password =? where username=?");
+           stmt.setString(1, fullname);
+           stmt.setString(2, password);
+	   stmt.setString(3, username);
+           stmt.execute();
+           System.out.println("User "+username+" updated successfully.");
+           return;
+	   }
+	}
+    }
+
+	   public void deleteUser(String username) throws SQLException {
+	      System.out.println();
+              if(existingUser(username) == false){
+                 System.out.println("No such user.");
+              return;
+              } else {
+	      PreparedStatement stmt = connection.prepareStatement("delete from users where username=?");
+	      stmt.setString(1, username);
+	      stmt.execute();
+	      System.out.println("Deleted");
+	      return;
+	      }
+	   }
+    //Closes Connection
     public void close() throws SQLException {
 	connection.close();
     }
@@ -60,11 +139,11 @@ public class DB {
     public static void demo() throws Exception {
 	DB db = new DB();
 	db.connect();
-	db.execute("update users set password=? where username=?",
-		   new String[] {"monkey", "fred"});
-	ResultSet rs = db.execute("select username,password,full_name from users");
-	while(rs.next()) System.out.println(rs.getString(1)+","+rs.getString(2)+","+rs.getString(3));
-	rs.close();
+//	db.updateUser("update users set password=? where username=?",
+//		   new String[] {"monkey", "fred"});
+//	db.listUsers();//"select username,password,full_name from users");
+//	while(rs.next()) System.out.println(rs.getString(1)+","+rs.getString(2)+","+rs.getString(3));
+//	rs.close();
 	db.close();
     }
 }
---
+++ src/main/java/edu/au/cc/gallery/tools/UserAdmin.java
@@ -0,0 +1,77 @@
+package edu.au.cc.gallery;
+/////
+import java.util.*;
+import java.io.*;
+import java.sql.*;
+
+public class UserAdmin {
+
+	private static Scanner s = new Scanner(System.in);
+
+	private static int promptUser() {
+	System.out.println(
+	     "1) List users\n"
+	   + "2) Add user\n"
+	   + "3) Edit user\n"
+	   + "4) Delete user\n"
+	   + "5) Quit");
+	   System.out.print("Enter command> ");
+	   int command = Integer.parseInt(s.nextLine());
+	   System.out.print("\n");
+	   return command;
+	}
+
+        public static void main (String [] args) {
+        DB db = new DB();
+        try {
+    	   db.connect();
+	   int i = promptUser();
+
+	   //Calls the "helpful" command from input
+	   switch(i){
+	   case 1: db.listUsers();
+		   break;
+	   case 2: System.out.print("Username> ");
+		   String username = s.nextLine();
+                   System.out.print("\nPassword > ");
+                   String password = s.nextLine();
+                   System.out.print("\nFull name > ");
+                   String fullname = s.nextLine();
+	           db.addUser(username, password, fullname);
+		   break;
+	   case 3: System.out.print("Username to edit> ");
+		   String updateUsername = s.nextLine();
+                   System.out.print("\nNew Password (press enter to keep current)> ");
+                   String updatePassword = s.nextLine();
+                   System.out.print("\nNew Full name (press enter to keep current)> ");
+                   String updateFullname = s.nextLine();
+		   db.updateUser(updateUsername, updatePassword, updateFullname);
+		   break;
+	   case 4: System.out.print("Enter username to delete> ");
+		   String deleteUsername = s.nextLine();
+		   System.out.print("Are you sure that you want to delete "+deleteUsername+"? [Yes/No]");
+		   String confirmation = s.next().toLowerCase();
+		   if(confirmation.equals("yes")){
+			db.deleteUser(deleteUsername);
+		   } else if (confirmation.equals("no")) {
+			System.out.println("Delete command cancelled.");
+		   } else {
+			System.out.print("Are you sure that you want to delete "+deleteUsername+"? [Yes/No]");
+                   	confirmation = s.next().strip().toLowerCase();
+		        }
+		   break;
+	   case 5: db.close();
+		   System.out.println("Bye.");
+		   break;
+   	   default:System.out.println("Invalid command. Please enter command from 1-5.");
+		   break;
+	   }
+
+	} catch (Exception e) {
+	   System.out.println(e);
+	}
+
+   }
+}
+
+
